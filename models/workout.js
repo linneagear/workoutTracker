b1dc0155a6e3acbdc281b15d43791e77165206fd
Linnea <linneagear@gmail.com>
2020-07-28 19:29:07 -0700

    Added html routes to render page, unsure if successful yet

--- models/workout.js
+++ models/workout.js
@@ -5,36 +5,44 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
 // this will create the collection/schema to the database
+// matches the seed.js layout
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter your workout type."
-  },
-  duration: {
-    type: Number,
-    required: "Enter the duration in minutes."
-  },
-  distance: {
-    type: Number,
-    required: "Enter the distance in miles."
-  },
-  weight: {
-    type: Number,
-    required: "Enter the weight in lbs."
-  },
-  sets: {
-    type: Number,
-    required: "Enter the number of sets."
-  },
-  reps: {
-    type: Number,
-    required: "Enter the number of reps."
-  },
-  date: {
+  day: {
     type: Date,
     default: Date.now
-  }
+  },
+  exercises: [{
+    type: {
+      type: String,
+      trim: true,
+      required: "Enter your workout type."
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter your workout name."
+    },
+    duration: {
+      type: Number,
+      required: "Enter the duration in minutes."
+    },
+    distance: {
+      type: Number,
+      required: "Enter the distance in miles."
+    },
+    weight: {
+      type: Number,
+      required: "Enter the weight in lbs."
+    },
+    sets: {
+      type: Number,
+      required: "Enter the number of sets."
+    },
+    reps: {
+      type: Number,
+      required: "Enter the number of reps."
+    }
+  }]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,41 +1,81 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-// post the body to api/workouts
-router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
+// ***** HTML ROUTES*****//
+
+// displays whatever page is called to that route
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'stats.html'));
+});
+
+// **** API ROUTES **** // 
+
+// get the workouts data
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+    // sort by last known date, and limit to one day
+    .sort({ date: -1})
+    .limit(1)
     .then(dbWorkout => {
         res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
+
+// get range of workouts
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout)
     })
     .catch(err => {
         res.json(err);
     });
 });
 
-// routers to get and post data
-router.get("/api/workouts", (req, res) => {
-    Workout.find({})
+// post any new workouts by using post and create
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
     .then(dbWorkout => {
-        res.json(dbWorkout);
+        res.json(dbWorkout)
     })
     .catch(err => {
-        res.status(400).json(err);
-    })
-});
-
-router.get("/api/exercise", ({ body }, res) => {
-    Workout.insertMany(body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.status(400).json(err);
+        res.json(err)
     });
 });
 
-router.post("/api/workouts/range", (req, res) => {
-    Workout.
-})
+// update by ID, using MongoDB documentation
+router.put("/api/workouts/:id", (req, res) => {
+    // we only want to update ONE document, at this id, with these new updates
+    Workout.updateOne( {_id: req.params.id}, {$push: {
+        exercises: [
+            "type": req.body.type,
+            "name": req.body.name,
+            "duration": req.body.duration,
+            "distance": req.body.distance,
+            "weight": req.body.weight,
+            "sets": req.body.sets,
+            "reps": req.body.reps
+        ]
+    }
+    })
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.json(err)
+    });
+});
 
 // export the router so it can be used
 module.exports = router;
