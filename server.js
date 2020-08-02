4822ac661b2a927a20606bd27e091162f91ed743
Linnea <linneagear@gmail.com>
2020-08-02 09:35:19 -0700

    Pushed html routes to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,8 +17,24 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", { useNewUrlParser: true });
 
 // set up routes
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes"));
+
+// ***** HTML ROUTES*****//
+
+// displays home page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// displays exercise page
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, './public/exercise.html'));
+});
+
+// displays stats page
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, './public/stats.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
